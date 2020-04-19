258775b443cfc800e4ba3a19eda83179ab7915cd
안현모 <blue_pope@hotmail.com>
2020-04-20 02:04:09 +0900

    초기 커밋.

---
+++ MyWeb.HomeWeb/Controllers/ViewComponents/LeftMenu.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWeb.HomeWeb.Controllers.ViewComponents
+{
+    public class LeftMenu : ViewComponent
+    {
+        public LeftMenu()
+        {
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
