1e9aa89e69483f8a9bd6a9be01c1561b193b0da7
안현모 <blue_pope@hotmail.com>
2020-08-02 18:53:43 +0900

    webapi 예제

---
+++ MyWeb.ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+namespace MyWeb.ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HttpClientTest();
+            RestSharpTest();
+        }
+
+        static void HttpClientTest()
+        {
+            var httpClient = new HttpClient();
+            var request = new HttpRequestMessage();
+            request.Method = HttpMethod.Get;
+
+            request.RequestUri = new Uri("https://localhost:44371/weatherforecast");
+            //request.Content = new FormUrlEncodedContent();
+
+            var response = httpClient.SendAsync(request).GetAwaiter().GetResult();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string responseText = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                Console.WriteLine(responseText);
+
+                var list = JsonConvert.DeserializeObject<List<WeatherForecastModel>>(responseText);
+                
+                foreach(var item in list)
+                {
+                    Console.WriteLine(item.Date);
+                }
+                
+            }
+            else
+            {
+                string responseText = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                Console.WriteLine(response.StatusCode);
+                Console.WriteLine(responseText);
+            }
+        }
+
+        static void RestSharpTest()
+        {
+            var restClient = new RestSharp.RestClient();
+            var request = new RestRequest();
+
+            request.Resource = "https://localhost:44371/weatherforecast";
+            request.Method = Method.GET;
+            //request.RequestFormat = DataFormat.None;
+
+            request.AddParameter("num", 4, ParameterType.GetOrPost);
+
+            var response = restClient.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string responseText = response.Content;
+
+
+                var list = JsonConvert.DeserializeObject<List<WeatherForecastModel>>(responseText);
+
+                Console.WriteLine(list.Count);
+
+            }
+            else
+            {
+                string responseText = response.Content;
+
+                Console.WriteLine(response.StatusCode);
+                Console.WriteLine(responseText);
+            }
+        }
+    }
+}
---
+++ MyWeb.HomeApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyWeb.HomeApi.Models;
+
+namespace MyWeb.HomeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Read
+        [HttpGet]
+        public IEnumerable<WeatherForecastModel> Get(int num)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, num).Select(index => new WeatherForecastModel
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        //Insert
+        [HttpPost]
+        public WeatherForecastModel Post([FromForm]int addDay)
+        {
+            return new WeatherForecastModel() { Date = DateTime.Now.AddDays(addDay) };
+        }
+
+        //Update
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        //Delete
+        [HttpDelete]
+        public IActionResult Delete(int key)
+        {
+            // key 처리 를 했다
+
+            //return Ok();
+
+            return BadRequest(new { message = "잘못된 요청입니다" });
+        }
+
+    }
+}
